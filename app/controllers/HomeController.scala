fb647f056712bedcae4dcee9f2357bfa5ed7474f
Lindsey <lindsey.dew@deliveroo.co.uk>
2018-11-14 20:52:48 +0200

    Filter out internal groups

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -11,31 +11,30 @@ import org.apache.kafka.streams.KafkaStreams.State
 import org.apache.kafka.streams.{KafkaStreams, KeyValue}
 import org.apache.kafka.streams.kstream.Windowed
 import org.apache.kafka.streams.state.{KeyValueIterator, QueryableStoreTypes}
+import util.KafkaUtils
 
 import scala.collection.JavaConverters._
 
 /**
-  * This controller creates an `Action` to handle HTTP requests to the
-  * application's home page.
-  */
+ * This controller creates an `Action` to handle HTTP requests to the
+ * application's home page.
+ */
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents, kafka: KafkaTask) extends AbstractController(cc) {
 
   /**
-    * Create an Action to render an HTML page.
-    *
-    * The configuration in the `routes` file means that this method
-    * will be called when the application receives a `GET` request with
-    * a path of `/`.
-    */
+   * Create an Action to render an HTML page.
+   *
+   * The configuration in the `routes` file means that this method
+   * will be called when the application receives a `GET` request with
+   * a path of `/`.
+   */
 
-  private def getContentDetails(
-                                 iterator: Seq[KeyValue[Windowed[String], ActiveGroup]]
+
+  private def getContentDetails(iterator: Seq[KeyValue[Windowed[String], ActiveGroup]]
                                ): Map[String, Seq[(Windowed[String], ClientDetails, Map[Topic, Set[ConsumerInstanceDetails]])]] = {
     iterator.map(itr => {
-      val consumerPerTopic: Map[Topic, Set[ConsumerInstanceDetails]] = itr.value.clientDetails.members.flatMap(m =>
-        m.assignedPartitions.map(_._1).map(_ -> m)
-      ).groupBy(_._1).mapValues(v=> v.map(_._2))
+      val consumerPerTopic= KafkaUtils.groupPerTopic(itr.value.clientDetails)
       val window = itr.key
       val clientDetails = itr.value.clientDetails
       (window, clientDetails, consumerPerTopic)
@@ -43,7 +42,7 @@ class HomeController @Inject()(cc: ControllerComponents, kafka: KafkaTask) exten
       s.sortWith({case(a,b) =>
         a._1.window().start > b._1.window().start
       }).headOption.toSeq
-      )
+    ).filterKeys(g => !g.startsWith("_"))
   }
 
   private def getWindowsBetween(streams: KafkaStreams, from: Long, to: Long) = {
